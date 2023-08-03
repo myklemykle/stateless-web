072222efaada1d916bb983fd706d24a460294236
Mykle Hansen <mykle@mykle.com>
2023-08-03 11:52:55 -0700

    react nav header with svg

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,10 +4,10 @@ import React from 'react'
 export default class Nav extends React.Component {
   render() {
     return (
-			<nav class="nav">
-				<a class="nav-link mx-1" href="#"><img src="assets/1x_idle.png" /></a>
-				<a class="nav-link mx-1" href="#"><img src="assets/call_new_selection.png" /></a>
-				<a class="nav-link mx-1" href="#"><img src="assets/4x_active.png" /></a>
+			<nav className="nav">
+				<a className="nav-link mx-1" href="#"><img src={require("../assets/1x_idle.svg")} /></a>
+				<a className="nav-link mx-1" href="#"><img src={require("../assets/call_new_selection.svg")} /></a>
+				<a className="nav-link mx-1" href="#"><img src={require("../assets/4x_active.svg")} /></a>
 			</nav>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+import React from 'react';
+
 import { createRoot } from 'react-dom/client';
 
 import Nav from './components/Nav';
 
 const navTop = document.getElementById('nav-top');
 const navTopRoot = createRoot(navTop);
-navTop.render(<Nav />);
+navTopRoot.render(<Nav />);
 
-const navBottomRoot = createRoot(navBottom);
 const navBottom = document.getElementById('nav-bottom');
-navBottom.render(<Nav  />);
+const navBottomRoot = createRoot(navBottom);
+navBottomRoot.render(<Nav  />);
 
